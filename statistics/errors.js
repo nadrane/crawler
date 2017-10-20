c727e615ee2d605b951d8db699ce8c24f48f9050
Nicholas Drane <nicholasdrane@gmail.com>
2017-10-20 13:09:30 -0500

    FIX(statistics) - retrieve anthing error level

---
+++ statistics/errors.js
@@ -0,0 +1,4 @@
+const readLog = require("./utils");
+const enexpectedErrors = readLog().filter(line => line.level === 50);
+
+console.log(enexpectedErrors);
--- statistics/unexpected-errors.js
+++
@@ -1,4 +0,0 @@
-const readLog = require("./utils");
-const enexpectedErrors = readLog().filter(line => line.event === "unexpected error");
-
-console.log(enexpectedErrors);
