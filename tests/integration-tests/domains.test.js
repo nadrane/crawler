2804dcc9cc4db99a0487da377ba552ee40156c97
Nicholas Drane <nicholasdrane@gmail.com>
2017-11-11 20:45:28 -0600

    add some basic integration tests against the domains module

---
+++ tests/integration-tests/domains.test.js
@@ -0,0 +1,21 @@
+const mocha = require("mocha");
+const { expect } = require("chai");
+const { promisify } = require('util')
+const rimraf = promisify(require('rimraf'))
+const { FRONTIER_DIRECTORY } = require('APP/env/')
+
+describe.only("Domains", () => {
+  let domains;
+  beforeEach(async () => {
+    await rimraf(FRONTIER_DIRECTORY + '/*')
+    domains = await require('APP/src/domains');
+  });
+
+  describe("getNextUrlToScrape", async () => {
+    it('returns a sequence of different urls from the seed file', async () =>{
+      expect(await domains.getNextUrlToScrape()).to.equal('http://google.com')
+      expect(await domains.getNextUrlToScrape()).to.equal('http://youtube.com')
+      expect(await domains.getNextUrlToScrape()).to.equal('http://facebook.com')
+    })
+  })
+})
