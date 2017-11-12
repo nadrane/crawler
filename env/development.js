44779a446463b9d6974e28dbdb9af9d4dbc4aed9
Nicholas Drane <nicholasdrane@gmail.com>
2017-11-11 20:18:39 -0600

    use absolute path in env file

--- env/development.js
+++ env/development.js
@@ -1,12 +1,12 @@
 const { promisify } = require('util')
 const { readFile } = require('fs');
+const path = require('path')
 const readFileAsync = promisify(readFile)
-
 // Since we need to hit the s3 api asynchronously (I know, total BS that we can't hit it synchronously) in prod,
 // let's also wrap the dev environment in a promise to provide a common api.
 
 module.exports = {
   LOGENTRIES_TOKEN_PROMISE: Promise.resolve(require("../logentries-credentials").DEV_TOKEN),
   SEED_FILE_PROMISE: readFileAsync('./seed-domains.txt'),
-  FRONTIER_DIRECTORY: "./frontiers"
+  FRONTIER_DIRECTORY: path.resolve(__dirname, "../frontiers")
 };
