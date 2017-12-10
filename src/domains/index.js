31a3527a5615babe830988bce1adef654db49ba8
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-10 00:06:49 -0600

    refactor(domains): refactor as a readable stream

--- src/domains/index.js
+++ src/domains/index.js
@@ -1,4 +1,49 @@
-const domainFactory = require('./domains');
-const { SEED_FILE_PROMISE } = require('APP/env')
+const { SEED_FILE_PROMISE } = require("APP/env");
+const { Readable } = require("stream");
+const domainFactoryPromise = require("./domains")(SEED_FILE_PROMISE);
 
-module.exports = domainFactory(SEED_FILE_PROMISE)
+class DomainReaderStream extends Readable {
+  constructor(domainFactory, concurrency) {
+    super({ objectMode: true });
+    const outstandingRequests = 0;
+    this.concurrency = concurrency;
+    this.buffer = []
+    this.domainFactory = domainFactory
+  }
+
+  _read(size) {
+    if (!this.domainFactory) {
+      return
+    }
+    if (this.running > this.concurrency) {
+      return
+    }
+    if (this.buffer.length) {
+      const url = this.buffer.shift();
+      const isBackPressure = !this.push(url)
+      if (isBackPressure) {
+        this.buffer.unshift(url)
+      }
+      return
+    }
+    this.outstandingRequests++;
+    this.domainFactory
+      .getNextUrlToScrape()
+      .then(url => {
+        this.running--;
+        const isBackPressure = !this.push(url + '\n')
+        if (isBackPressure) {
+          this.buffer.push(url)
+        }
+      })
+      .catch(err => {
+        this.running--;
+        return this.emit("error", err);
+      });
+  }
+}
+
+module.exports = domainFactoryPromise
+  .then(domainFactory => {
+    return new DomainReaderStream(domainFactory)
+  })
