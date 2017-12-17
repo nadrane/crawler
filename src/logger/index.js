cc0f737a71c773d48eaaef28304a103c7ee85742
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-16 21:20:49 -0600

    revert(logger): turns out I had that factory to customize the log file

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,5 +1,10 @@
-const loggerCreator = require("./core-logger");
-const bunyanFactory = require("./buyan-adaptor");
+const loggerCreator = require('./core-logger')
+const bunyanFactory = require('./buyan-adaptor')
 
-logger = loggerCreator(bunyanFactory, outputFile);
-module.exports = logger;
+let logger
+module.exports = function(outputFile) {
+  if (!logger) {
+    logger = loggerCreator(bunyanFactory, outputFile)
+  }
+  return logger
+}
