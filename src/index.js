272b3b779f66ea23e0ca452623f4660f4bf3fe86
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-17 09:15:13 -0600

    fix(index): read concurrency from stdin and set edfault

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ const path = require("path");
 const { c, o } = argv; // maximum file descriptors open | output file name
 const logger = require("./logger")("temp-log.txt");
 
-const concurrency = 5;
+c = c || 5; // defalt concurrency
 
-const robotsStream = require("./robots-parser/")(concurrency);
+const robotsStream = require("./robots-parser/")(c);
 robotsStream.on("error", err => {
   console.error('robots stream error', err)
 })
 
-const requestStream = require("./requester/")(concurrency);
+const requestStream = require("./requester/")(c);
 requestStream.on("error", err => {
   console.error('requests stream error', err)
 })
 
-require("./domains")(concurrency).then(domainReader => {
+// I should really fix this.
+// domains should not return a promise.
+// We need to pass in the env config (seed file) to file this
+// That way we can pull the async handling all out here
+require("./domains")(c).then(domainReader => {
   domainReader.on("error", err => {
     console.error('domain reader stream error', err)
   })
