5b5bff0cc70481a0fb9d7c7237f53634d4fa837a
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-16 21:15:46 -0600

    refacter(logger): get rid of stupid improperly implemented singleton

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,10 +1,5 @@
-const loggerCreator = require('./core-logger')
-const bunyanFactory = require('./buyan-adaptor')
+const loggerCreator = require("./core-logger");
+const bunyanFactory = require("./buyan-adaptor");
 
-let logger
-module.exports = function(outputFile) {
-  if (!logger) {
-    logger = loggerCreator(bunyanFactory, outputFile)
-  }
-  return logger
-}
+logger = loggerCreator(bunyanFactory, outputFile);
+module.exports = logger;
