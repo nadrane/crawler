d8d01f70ab710a338e4594a502b1de0b0686bee9
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-20 10:05:46 -0600

    refactor(requester): use more obvious static class

--- src/requester/requester.js
+++ src/requester/requester.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 const { parse } = require("tldjs");
 const { USER_AGENT } = require("APP/env");
 
-class Requester {
+const Requester = {
   async crawlWithGetRequest(url) {
     logger.GETRequestSent(url, this.totalRequestsMade);
-    let response
+    let response;
     try {
       response = await axios({
         method: "get",
@@ -16,40 +16,36 @@ class Requester {
           userAgent: USER_AGENT
         }
       });
-      // logger.GETResponseReceived(url, response.status);
-      //TODO emit finished event. store url in bloom filter
-      // Note we could easily hit the same url twice since we are choosing to keep
-      // no record of urls currently being processed, though this is SUPER unlikely (if not impossible)
-      // becuase of domain throttling
+      logger.GETResponseReceived(url, response.status);
     } catch (err) {
-      this._failedRequest(err);
+      this._failedRequest(err, url);
     }
-    return response
+    return response;
   }
+};
 
-  _failedRequest(err) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    if (err.response) {
-      const { headers, status } = err.response;
-      logger.GETResponseError(url, err, status, headers);
-      // No response received
-    } else if (err.request) {
-      // do not retry if connection reset
-      // simply limiting complexity here
-      if (err.code === "ECONNRESET") {
-        const { domain } = parse(url);
-        logger.connectionReset(url);
-      } else {
-        logger.noGETResponseRecieved(err, url);
-      }
+function failedRequest(err, url) {
+  // The request was made and the server responded with a status code
+  // that falls out of the range of 2xx
+  if (err.response) {
+    const { headers, status } = err.response;
+    logger.GETResponseError(url, err, status, headers);
+    // No response received
+  } else if (err.request) {
+    // do not retry if connection reset
+    // simply limiting complexity here
+    if (err.code === "ECONNRESET") {
+      const { domain } = parse(url);
+      logger.connectionReset(url);
     } else {
-      logger.unexpectedError(err, "bad request", {
-        module: "get request",
-        config: err.config
-      });
+      logger.noGETResponseRecieved(err, url);
     }
+  } else {
+    logger.unexpectedError(err, "bad request", {
+      module: "get request",
+      config: err.config
+    });
   }
 }
 
-module.exports = new Requester();
+module.exports = Requester;
