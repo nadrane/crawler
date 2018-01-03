7fbafe4fc51b1650a024623accf56a4d142f0f23
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-02 21:57:04 -0600

    tests(frontier): update to use dependency injection

--- tests/unit-tests/domains/frontier.test.js
+++ tests/unit-tests/domains/frontier.test.js
@@ -1,55 +1,51 @@
-const fs = require("fs");
 const sinon = require("sinon");
-const mocha = require("mocha");
 const { expect } = require("chai");
 const Frontier = require("APP/src/domains/frontier");
 
 const sandbox = sinon.createSandbox();
 
-describe("Frontier", () => {
-  let fileWriter,
-    fileAppender;
+describe.only("Frontier", () => {
+  let storage = {};
   beforeEach(() => {
-    fileWriter = sandbox.stub(fs, "writeFileSync");
-    fileAppender = sandbox.stub(fs, "appendFileAsync");
-  });
-  afterEach(() => {
-    sandbox.restore();
+    storage.writeFileSync = sinon.spy();
+    storage.readFileAsync = sinon.spy();
+    storage.writeFileAsync = sinon.spy();
+    storage.appendFileAsync = sinon.spy();
   });
   describe("isEmpty", () => {
     it("should return false when the frontier is not empty", () => {
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       expect(frontier.isEmpty()).to.be.false;
     });
 
     it("should return true when the frontier is empty", () => {
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       frontier.urlsInFrontier = 0;
       expect(frontier.isEmpty()).to.be.true;
     });
   });
   describe("readyForReading", () => {
     it("should be ready for reading upon creation", () => {
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       expect(frontier.readyForReading()).to.be.true;
     });
 
     it("should not be ready for reading when the frontier is empty", () => {
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       frontier.urlsInFrontier = 0;
       expect(frontier.readyForReading()).to.be.false;
     });
   });
   describe("append", () => {
     it("queues up a link to add to the frontier", () => {
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       frontier.append("www.yahoo.com");
       expect(frontier.queuedNewlinks).lengthOf(1);
     });
 
     it("calls flushNewLinkQueue after one minute", () => {
       const clock = sinon.useFakeTimers();
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       const newLinkQueueSpy = sinon.spy(frontier, "flushNewLinkQueue");
       frontier.append("www.yahoo.com");
 
@@ -64,7 +60,7 @@ describe("Frontier", () => {
 
     it("does not schedule flushNewLinkQueue multiple times given multiple calls within one minute", () => {
       const clock = sinon.useFakeTimers();
-      const frontier = new Frontier("www.google.com");
+      const frontier = new Frontier("www.google.com", storage);
       const newLinkQueueSpy = sinon.spy(frontier, "flushNewLinkQueue");
       frontier.append("www.yahoo.com");
       frontier.append("www.microsoft.com");
@@ -79,11 +75,9 @@ describe("Frontier", () => {
     });
   });
   describe("flushNewLinkQueue", () => {
-    let frontier,
-      newLinkQueueSpy,
-      clock;
+    let frontier, newLinkQueueSpy, clock;
     beforeEach(() => {
-      frontier = new Frontier("www.google.com");
+      frontier = new Frontier("www.google.com", storage);
       newLinkQueueSpy = sinon.spy(frontier, "flushNewLinkQueue");
       clock = sinon.useFakeTimers();
       frontier.flushScheduled = true;
@@ -118,9 +112,13 @@ describe("Frontier", () => {
     it("appends the queued links to the frontier file", async () => {
       frontier.currentlyReading = false;
       await frontier.flushNewLinkQueue();
-      expect(fileAppender.calledOnce).to.be.true;
-      expect(fileAppender.calledWithExactly(frontier.fileName, "www.microsoft.com\nwww.yahoo.com\n")).to
-        .be.true;
+      expect(storage.appendFileAsync.calledOnce).to.be.true;
+      expect(
+        storage.appendFileAsync.calledWithExactly(
+          frontier.fileName,
+          "www.microsoft.com\nwww.yahoo.com\n"
+        )
+      ).to.be.true;
       expect(frontier.urlsInFrontier).to.equal(3);
       expect(frontier.flushScheduled).to.be.false;
       expect(frontier.queuedNewlinks).to.be.empty;
@@ -128,12 +126,14 @@ describe("Frontier", () => {
   });
   describe("getNextUrl", () => {
     it("returns the first url in the frontier", async () => {
-      sandbox.stub(fs, "writeFileAsync").returns(Promise.resolve());
-      frontier = new Frontier("www.bing.com");
-      frontier.urlsInFrontier = 3;
-      const fileReader = sinon
-        .stub(fs, "readFileAsync")
+      storage.writeFileAsync = sinon.stub().returns(Promise.resolve());
+      storage.readFileAsync = sinon
+        .stub()
         .returns("www.bing.com\nwww.bing.com/link1\nwww.bing.com/link2");
+
+      frontier = new Frontier("www.bing.com", storage);
+      frontier.urlsInFrontier = 3;
+
       expect(await frontier.getNextUrl()).to.equal("www.bing.com");
       expect(frontier.urlsInFrontier).to.equal(2);
       expect(frontier.currentlyReading).to.be.false;
