4a96386c0a9f5303bdfdcd9181c09e82081d2706
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-03 12:14:44 -0600

    test(domains): account for dependency injected storage

--- tests/unit-tests/domains/domains.test.js
+++ tests/unit-tests/domains/domains.test.js
@@ -1,35 +1,34 @@
 const fs = require("fs");
 const sinon = require("sinon");
 const { expect } = require("chai");
-const domainFactory = require("APP/src/domains/domains");
-
-const sandbox = sinon.createSandbox();
+const Domains = require("APP/src/domains/domains");
+const Events = require("events");
 
 describe("Domains", () => {
-  let domainsPromise;
-  let domains;
+  const storage = {};
   beforeEach(() => {
-    sandbox.stub(fs, "writeFileSync");
-    sandbox.stub(fs, "writeFileAsync");
-    sandbox.stub(fs, "readFileAsync").returns(Promise.resolve("www.google.com"));
-  });
-  afterEach(() => {
-    sandbox.restore();
+    storage.writeFileSync = sinon.spy();
+    storage.readFileAsync = sinon.spy();
+    storage.writeFileAsync = sinon.spy();
+    storage.appendFileAsync = sinon.spy();
   });
 
   describe("getDomainToScrape", () => {
-    it("should return the next domain that is polite to scrape and empty string if there are no polite options", async () => {
-      const domainsPromise = Promise.resolve(Buffer.from("www.google.com\nwww.yahoo.com\nwww.bing.com"));
-      const domains = await domainFactory(domainsPromise);
+    it("should return the next domain that is polite to scrape and empty string if there are no polite options", () => {
+      const eventCoordinator = new Events();
+      const seed = ["www.google.com", "www.yahoo.com", "www.bing.com"];
+      const domains = new Domains(seed, eventCoordinator, storage);
 
       expect(domains.getDomainToScrape()).to.equal("www.google.com");
       expect(domains.getDomainToScrape()).to.equal("www.yahoo.com");
       expect(domains.getDomainToScrape()).to.equal("www.bing.com");
     });
 
-    it("should reset to the beginning of the generator when depleted", async () => {
-      const domainsPromise = Promise.resolve(Buffer.from("www.google.com"));
-      const domains = await domainFactory(domainsPromise);
+    it("should reset to the beginning of the generator when depleted", () => {
+      const eventCoordinator = new Events();
+      const seed = ["www.google.com"];
+      const domains = new Domains(seed, eventCoordinator, storage);
+
       const clock = sinon.useFakeTimers();
       const twoMinutes = 60 * 2 * 1000;
       clock.tick(twoMinutes); // this is necessary because time starts at 0 in Sinon
@@ -43,16 +42,18 @@ describe("Domains", () => {
   });
 
   describe("countOpenFiles", () => {
-    it("should return zero when no files are being read", async () => {
-      const domainsPromise = Promise.resolve(Buffer.from("www.google.com"));
-      const domains = await domainFactory(domainsPromise);
+    it("should return zero when no files are being read", () => {
+      const eventCoordinator = new Events();
+      const seed = ["www.google.com"];
+      const domains = new Domains(seed, eventCoordinator, storage);
 
       expect(domains.countOpenFiles()).to.equal(0);
     });
 
-    it("should correctly count the number of open files", async () => {
-      const domainsPromise = Promise.resolve(Buffer.from("www.google.com\nwww.yahoo.com"));
-      const domains = await domainFactory(domainsPromise);
+    it("should correctly count the number of open files", () => {
+      const eventCoordinator = new Events();
+      const seed = ["www.google.com", "www.yahoo.com"];
+      const domains = new Domains(seed, eventCoordinator, storage);
 
       domains.getNextUrlToScrape();
       expect(domains.countOpenFiles()).to.equal(1);
@@ -63,10 +64,18 @@ describe("Domains", () => {
 
   describe("getNextUrlToScrape", () => {
     it("returns the urls from the file", async () => {
-      const domainsPromise = Promise.resolve(Buffer.from("www.google.com\nwww.yahoo.com"));
-      const domains = await domainFactory(domainsPromise);
+      const eventCoordinator = new Events();
+      const seed = ["www.google.com", "www.yahoo.com"];
+      const domains = new Domains(seed, eventCoordinator, storage);
+      storage.readFileAsync = sinon
+        .stub()
+        .onFirstCall()
+        .returns("www.google.com")
+        .onSecondCall()
+        .returns("www.yahoo.com");
 
       expect(await domains.getNextUrlToScrape()).to.equal("www.google.com");
+      expect(await domains.getNextUrlToScrape()).to.equal("www.yahoo.com");
     });
   });
 });
