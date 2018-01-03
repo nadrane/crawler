04fcd8a0ef10d817c08c1fad9303b6d5fcc743b3
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-03 12:07:07 -0600

    style(domains): formatting changes. +removing useless await call

--- src/domains/domains.js
+++ src/domains/domains.js
@@ -2,17 +2,20 @@ const DomainTracker = require("./domain-tracker");
 const { parse } = require("tldjs");
 
 class Domains {
-  constructor(seedData, eventCoordinator) {
+  constructor(seedData, eventCoordinator, storage) {
     this.domainTrackers = new Map();
+    this.storage = storage;
     this.seedDomains(seedData);
     this.domainGenerator = this._nextDomain();
-    eventCoordinator.on("new link", (url) => {
+    eventCoordinator.on("new link", url => {
       this.appendNewUrl(url);
     });
   }
 
   seedDomains(seedData) {
-    seedData.map(domain => this.domainTrackers.set(domain, new DomainTracker(domain)));
+    seedData.map(domain =>
+      this.domainTrackers.set(domain, new DomainTracker(domain, this.storage))
+    );
   }
 
   appendNewUrl(url) {
@@ -32,7 +35,7 @@ class Domains {
     return filesOpen;
   }
 
-  * _nextDomain() {
+  *_nextDomain() {
     for (const [domain, domainTracker] of this.domainTrackers) {
       if (domainTracker.readyToScrape()) {
         yield domain;
@@ -49,13 +52,13 @@ class Domains {
     return domain.value;
   }
 
-  async getNextUrlToScrape() {
+  getNextUrlToScrape() {
     const domain = this.getDomainToScrape();
     if (!domain) {
       return "";
     }
     const domainTracker = this.domainTrackers.get(domain);
-    return await domainTracker.getNextUrl();
+    return domainTracker.getNextUrl();
   }
 }
 
