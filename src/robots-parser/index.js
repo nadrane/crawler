fa08482d24f8337b34049b8c9c88440669b426ad
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-06 09:52:32 -0600

    fix(robots stream): let stream error handler handle errors

--- src/robots-parser/index.js
+++ src/robots-parser/index.js
@@ -6,15 +6,11 @@ const isAllowed = makeRobotParser();
 
 module.exports = function createRobotsStream(concurrency) {
   return throughConcurrent("robots stream", concurrency, function getRobotsTxt(url, enc, done) {
-    isAllowed(url, axios)
-      .then(allowed => {
-        if (allowed) {
-          this.push(url);
-        }
-        done();
-      })
-      .catch(err => {
-        this.on("error", err);
-      });
+    isAllowed(url, axios).then(allowed => {
+      if (allowed) {
+        this.push(url);
+      }
+      done();
+    });
   });
 };
