3676ee559fe61cec7eb1a33e78a46b348b220679
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-06 14:41:35 -0600

    fix(stats server): response to http request! also track more data

--- statistics/server.js
+++ statistics/server.js
@@ -8,19 +8,26 @@ app.use(bodyParser.json());
 const stats = {};
 
 app.post("/log", (req, res) => {
+  console.log('got it')
   req.pipe(ndjson.parse()).on("data", line => {
     const { event, domain, hostname } = line;
     if (!stats.hasOwnProperty(hostname)) {
       stats[hostname] = {};
+      stats[hostname].total = 0;
     }
     if (!stats[hostname].hasOwnProperty(domain)) {
       stats[hostname][domain] = {};
+      stats[hostname][domain] = 0;
     }
     if (!stats[hostname][domain].hasOwnProperty(event)) {
       stats[hostname][domain][event] = 0;
     }
-    stats[hostname][domain][event] += 1;
+    if (event === "request sent") {
+      stats[hostname].total += 1;
+      stats[hostname][domain].total += 1;
+    }
   });
+  res.sendStatus(200);
 });
 
 app.get("/log", (req, res) => {
