0bf000e2a74fccd45c23e2e66eaa3fc3728fda16
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-07 16:00:50 -0600

    refactor(logger): stop using singleton pattern

--- src/logger/index.js
+++ src/logger/index.js
@@ -3,13 +3,12 @@ const Logger = require("./core-logger");
 const bunyanWithHTTPStream = require("./bunyan-adaptor");
 const { LOGGING_DIR } = require("APP/env/");
 
-let logger;
 module.exports = function createLogger(eventCoordinator, http, statServerUrl, inputtedOuputFile) {
-  let outputFile;
-  if (!logger) {
-    // if (!eventCoordinator) throw new Error("event coordinator expected");
-    outputFile = inputtedOuputFile || path.join(LOGGING_DIR, "logs.txt");
-    logger = new Logger(eventCoordinator, outputFile, bunyanWithHTTPStream(`${statServerUrl}/log`, http));
-  }
-  return logger;
+  if (!eventCoordinator) throw new Error("event coordinator expected");
+  const outputFile = inputtedOuputFile || path.join(LOGGING_DIR, "logs.txt");
+  return new Logger(
+    eventCoordinator,
+    outputFile,
+    bunyanWithHTTPStream(`${statServerUrl}/log`, http)
+  );
 };
