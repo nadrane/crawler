02cda7d79190b56ad88b5d3d8b066672b8baff9b
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-08 16:00:12 -0600

    refactor(requester): use async/await and inject logger

--- src/requester/index.js
+++ src/requester/index.js
@@ -1,52 +1,47 @@
-const path = require("path");
-const requester = require("./requester");
-const throughConcurrent = require("../through-concurrent");
-const Parser = require("../parser/");
-const logger = require("../logger/")();
-const sha1 = require("sha1");
 const AWS = require("aws-sdk");
+const makeRequester = require("./requester");
+const Parser = require("../parser/");
+const sha1 = require("sha1");
+const throughConcurrent = require("../through-concurrent");
 
-AWS.config.loadFromPath(path.join(__dirname, "..", "..", "aws-credentials.json"));
 const s3 = new AWS.S3();
 const s3Stream = require("s3-upload-stream")(s3);
 
-module.exports = function createRequesterStream(concurrency, eventCoordinator) {
-  return throughConcurrent("requester stream", concurrency, (requestUrl, enc, done) => {
-    requester
-      .crawlWithGetRequest(requestUrl)
-      .then(response => {
-        // In case the request failed
-        if (!response) return;
+module.exports = function createRequesterStream(logger, http, eventCoordinator, concurrency) {
+  const crawlWithGetRequest = makeRequester(logger, http);
+  return throughConcurrent("requester stream", concurrency, async (requestUrl, enc, done) => {
+    let htmlStream;
+    try {
+      htmlStream = await crawlWithGetRequest(requestUrl);
+    } catch (err) {
+      logger.unexpectError(err, "requester stream failure");
+    }
+    // In case the request failed
+    if (!htmlStream) return;
 
-        const htmlStream = response.data;
-        const responseClosed = new Promise(resolve => {
-          // the streams file descriptor should close on this event
-          // stream is an instance of httpClientRequest and does not email a close event
-          htmlStream.on("end", () => {
-            resolve();
-          });
-        });
-
-        const parserStream = new Parser(requestUrl, eventCoordinator);
-        htmlStream.pipe(parserStream);
-
-        const upload = s3Stream.upload({
-          Bucket: "crawler-nick",
-          Key: `sites/${sha1(requestUrl)}`
-        });
-        htmlStream.pipe(upload);
-        const uploadFinished = new Promise(resolve => {
-          upload.on("uploaded", () => {
-            resolve();
-          });
-        });
-        Promise.all([responseClosed, uploadFinished]).then(() => {
-          done();
-        });
-      })
-      .catch(err => {
-        logger.unexpectedError(err, "requester error");
-        done();
+    const responseClosed = new Promise(resolve => {
+      // the streams file descriptor should close on this event
+      // stream is an instance of httpClientRequest and does not email a close event
+      htmlStream.on("end", () => {
+        resolve();
       });
+    });
+
+    const parserStream = new Parser(requestUrl, eventCoordinator);
+    htmlStream.pipe(parserStream);
+
+    const upload = s3Stream.upload({
+      Bucket: "crawler-nick",
+      Key: `sites/${sha1(requestUrl)}`
+    });
+    htmlStream.pipe(upload);
+    const uploadFinished = new Promise(resolve => {
+      upload.on("uploaded", () => {
+        resolve();
+      });
+    });
+    Promise.all([responseClosed, uploadFinished]).then(() => {
+      done();
+    });
   });
 };
