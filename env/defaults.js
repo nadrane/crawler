3ee4ee9918d0ebedcf72e63413b4e76b3f82f52a
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-09 11:25:28 -0600

    fix(defaults): reduce max concurrency to something reasonable for standard network connections

--- env/defaults.js
+++ env/defaults.js
@@ -4,6 +4,6 @@ module.exports = {
   USER_AGENT: "test crawler - nickdrane.com/crawler",
   LOGGING_DIR: path.join(__dirname, "../logs"),
   DOMAIN_REQUEST_TIME_INTERVAL: 2 * 60 * 1000,
-  MAX_CONCURRENCY: 120,
-  BLOOM_FILTER_NAME: "crawler",
+  MAX_CONCURRENCY: 10,
+  BLOOM_FILTER_NAME: "crawler"
 };
