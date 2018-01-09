862b6644a4f860fade037dbf895d859b30ce092e
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-09 11:26:40 -0600

    refactor(dev config): load aws credentials. use server-info.json

--- env/development.js
+++ env/development.js
@@ -1,17 +1,21 @@
-const { promisify } = require("util");
-const { readFile } = require("fs");
+const AWS = require("aws-sdk");
 const path = require("path");
 
-const statsServers = require("../stats-server-credentials.json");
+AWS.config.loadFromPath(path.join(__dirname, "..", "aws-credentials.json"));
 
-// Since we need to hit the s3 api asynchronously (I know, total BS that we can't hit it synchronously) in prod,
+// Since we need to hit the seed file api asynchronously (I know, total BS that we can't hit it synchronously) in prod,
 // let's also wrap the dev environment in a promise to provide a common api.
 
 module.exports = {
-  STATS_SERVER_IP: Promise.resolve(statsServers.ipAddress),
-  SEED_FILE_PROMISE: new Promise((resolve, reject) => {
-    resolve(require("APP/seed-domains-sans-subs.json"))
+  SERVER_INFO: Promise.resolve({
+    statServerUrl: "localhost",
+    statServerPort: 8082,
+    bloomFilterUrl: "127.0.0.1"
+  }),
+  MAX_CONCURRENCY: 10,
+  SEED_FILE_PROMISE: new Promise(resolve => {
+    resolve(require("APP/seed-domains-sans-subs.json"));
   }),
   FRONTIER_DIRECTORY: path.resolve(__dirname, "../frontiers"),
-  DOMAIN_REQUEST_TIME_INTERVAL: 20 * 1000,
+  DOMAIN_REQUEST_TIME_INTERVAL: 20 * 1000
 };
