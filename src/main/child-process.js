ea2938dd86276a5daf901d9c50c02a481527b94c
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-09 11:27:35 -0600

    refactor(main): complete rewrite

--- src/main/child-process.js
+++ src/main/child-process.js
@@ -1,63 +1,41 @@
-const path = require("path");
-const mkdirp = require("mkdirp");
+const axios = require("axios");
+const fs = require("fs");
+// const enableHeapDumps = require("./heap-dumps");
+const makeBloomFilterSetStream = require("../bloom-filter/set-stream");
+const makeBloomFilterCheckStream = require("../bloom-filter/check-stream");
+const makeRobotsStream = require("../robots-parser/");
+const makeRequestStream = require("../requester/");
+const makeDomainStream = require("../domains/");
+// enableHeapDumps();
 
-const env = require("../../env/");
-
-function configureHeapDumps() {
-  if (env.isDev()) {
-    const heapdump = require("heapdump");
-    const debugDir = path.join(env.LOGGING_DIR, "heap-dumps");
-    mkdirp(debugDir);
-    setInterval(() => {
-      heapdump.writeSnapshot(path.join(debugDir, `${Date.now()}.heapsnapshot`));
-    }, 10 * 1000);
-  }
-}
-configureHeapDumps();
-
-function configureProcessErrorHandling(logger) {
-  process.on("uncaughtException", err => {
-    logger.unexpectedError(err, "uncaught exception");
-  });
-
-  process.on("unhandledRejection", (reason, p) => {
-    logger.unexpectedError(reason, "unhandled promise rejection", p);
-  });
-}
-
-function initialization(maxConcurrency, eventCoorindator) {
-  const logger = require("../logger")();
-  configureProcessErrorHandling(logger);
-
-  const bloomFilterCheckStream = require("../bloom-filter/check-stream")(maxConcurrency);
-  const bloomFilterSetStream = require("../bloom-filter/set-stream")(maxConcurrency);
-  const robotsStream = require("../robots-parser/")(maxConcurrency);
-  const requestStream = require("../requester/")(maxConcurrency, eventCoorindator);
-
+module.exports = async function initializeChildProcess(
+  logger,
+  eventCoorindator,
+  bloomFilterClient,
+  maxConcurrency
+) {
   const seedFile = new Promise(resolve => {
     process.on("message", data => {
       resolve(data);
     });
   });
 
-  seedFile
-    .then(seedData => {
-      const domainStream = require("../domains")(maxConcurrency, seedData, eventCoorindator);
+  let seedData;
+  try {
+    seedData = await seedFile;
+  } catch (err) {
+    console.error(err, "Could not retrieve seed data from server");
+  }
+  const domainStream = makeDomainStream(seedData, eventCoorindator, fs, logger, maxConcurrency);
+  const bloomFilterCheckStream = makeBloomFilterCheckStream(bloomFilterClient, logger, maxConcurrency);
+  const bloomFilterSetStream = makeBloomFilterSetStream(bloomFilterClient, logger, maxConcurrency);
+  const robotsStream = makeRobotsStream(logger, axios, maxConcurrency);
+  const requestStream = makeRequestStream(logger, axios, eventCoorindator, maxConcurrency);
 
-      domainStream.on("error", err => {
-        logger.unexpectedError(err, "domain stream");
-      });
-
-      domainStream
-        .pipe(bloomFilterCheckStream)
-        .pipe(robotsStream)
-        .pipe(bloomFilterSetStream) // notice we mark it visited before visiting. If we the request fails, it fails for good
-        .pipe(requestStream)
-        .pipe(process.stdout);
-    })
-    .catch(err => {
-      console.error("init error", err);
-    });
-}
-
-module.exports = initialization;
+  domainStream
+    .pipe(bloomFilterCheckStream)
+    .pipe(robotsStream)
+    .pipe(bloomFilterSetStream) // notice we mark it visited before visiting. If we the request fails, it fails for good
+    .pipe(requestStream)
+    .pipe(process.stdout);
+};
--- src/main/index.js
+++ src/main/index.js
@@ -1,86 +1,70 @@
 const cluster = require("cluster");
+const childProcess = require("child_process");
 const Events = require("events");
-const path = require("path");
-const numCPUs = require("os").cpus().length;
-const childProcess = require("./child-process");
+const initializeChildProcess = require("./child-process");
 const argv = require("minimist")(process.argv.slice(2));
 const axios = require("axios");
+const configureProcessErrorHandling = require("./error-handling");
+const { chunkByIndex } = require("../arrayUtilities");
+const makeBloomFilterClient = require("../bloom-filter/client");
+const makeLogger = require("../logger/");
+const numCPUs = require("os").cpus().length;
+const { MAX_CONCURRENCY, SEED_FILE_PROMISE, SERVER_INFO, isDev } = require("../../env/");
+const onDeath = require("death");
 
-const eventCoordinator = new Events();
-const { LOGGING_DIR, MAX_CONCURRENCY, SEED_FILE_PROMISE, STATS_SERVER_IP } = require("../../env/");
 const { n, c, o } = argv; // number of machines | maximum file descriptors open | output file name
 const numberOfMachines = n || 1;
 const maxConcurrency = c || MAX_CONCURRENCY;
-const logFile = o || path.join(LOGGING_DIR, "log.txt");
 
-process.on("uncaughtException", err => {
-  console.log(err, "uncaught exception");
-});
+let statServer;
+const workers = [];
 
-process.on("unhandledRejection", (reason, p) => {
-  console.log(reason, "unhandled promise rejection", p);
-});
+SERVER_INFO.then(async ({ statServerUrl, statServerPort, bloomFilterUrl }) => {
+  const eventCoordinator = new Events();
+  const logger = makeLogger(eventCoordinator, axios, { statServerUrl, statServerPort, outputFile: o });
+  const bloomFilterClient = makeBloomFilterClient(logger, bloomFilterUrl);
+  configureProcessErrorHandling(logger);
 
-let logger;
-STATS_SERVER_IP.then(statServerIp => {
-  logger = require("../logger/")(eventCoordinator, axios, statServerIp, logFile);
   if (cluster.isMaster) {
-    setupBloomFilter()
-    .then(() => SEED_FILE_PROMISE)
-    .then(seed => {
-      createChildren(chunkByIndex(seed));
-    });
+    configureServerTermination();
+    startStatServer(statServerUrl, statServerPort);
+    await bloomFilterClient.initializeBloomFilter();
+    const seed = await SEED_FILE_PROMISE;
+    console.log("seed file downloaded");
+    createChildren(logger, chunkByIndex(seed, numCPUs), bloomFilterClient);
   } else {
-    childProcess(maxConcurrency, eventCoordinator);
+    console.log("initializing child with pid", process.pid);
+    initializeChildProcess(logger, eventCoordinator, bloomFilterClient, maxConcurrency);
   }
 });
 
-
-async function setupBloomFilter() {
-  const bloomFilter = require("../bloom-filter/bloom-filter");
-  await bloomFilter.drop();
-  let tries = 0;
-  let success = false;
-  while (tries < 5 && !success) {
-    try {
-      console.log("attempting BF create");
-      await bloomFilter.create();
-      success = true;
-    } catch (err) {
-      console.log("BF create failed");
-      tries += 1;
-    }
-    await sleep(1000);
-  }
-  if (!success) {
-    throw new Error("failed to initialize bloom filter");
+function startStatServer(statServerUrl, statServerPort) {
+  if (isDev()) {
+    console.log("starting stat server");
+    statServer = childProcess.fork("./statistics/startServer", [statServerUrl, statServerPort], {
+      env: { NODE_ENV: process.env.NODE_ENV }
+    });
   }
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function createChildren(urlChunks) {
+function createChildren(logger, urlChunks) {
   for (let i = 0; i < numCPUs; i++) {
     const child = cluster.fork();
+    workers.push(child);
     logger.spawningWorkerProcess(child.pid);
     child.send(urlChunks[i]);
   }
 }
 
-function chunkByPredicate(seed, predicate) {
-  const chunks = [];
-  seed.forEach((url, index) => {
-    if (Array.isArray(chunks[predicate(index)])) {
-      chunks[predicate(index)].push(url);
-    } else {
-      chunks[predicate(index)] = [url];
+function configureServerTermination() {
+  onDeath(() => {
+    if (!statServer.killed) {
+      statServer.kill();
     }
+    workers.forEach(worker => {
+      if (worker.isConnected()) {
+        worker.disconnect();
+      }
+    });
   });
-  return chunks;
-}
-
-function chunkByIndex(seed) {
-  return chunkByPredicate(seed, index => index % numCPUs);
 }
