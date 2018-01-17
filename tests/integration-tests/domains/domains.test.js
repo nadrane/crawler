b637e1b9aea83b620545b06ec76b1fdd989a06ec
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-17 11:54:12 -0600

    test(domains): inject logger and fs

--- tests/integration-tests/domains/domains.test.js
+++ tests/integration-tests/domains/domains.test.js
@@ -1,11 +1,16 @@
-const Events = require('events');
+const Events = require("events");
+const fs = require("fs");
+const sinon = require("sinon");
+
 const { expect } = require("chai");
 const { promisify } = require("util");
 const rimraf = promisify(require("rimraf"));
 const { FRONTIER_DIRECTORY } = require("APP/env/");
 const Domains = require("APP/src/domains/domains");
+const makeLogger = require("APP/src/logger/");
 
 describe("Domains", () => {
+  let fsStub;
   beforeEach(async () => {
     await rimraf(`${FRONTIER_DIRECTORY}/*`);
   });
@@ -14,10 +19,12 @@ describe("Domains", () => {
   });
 
   describe("getNextUrlToScrape", async () => {
-    it("returns a sequence of different urls from the seed file", async () => {
+    it("returns a sequence of different urls", async () => {
       const eventCoordinator = new Events();
+      const http = { post: sinon.stub().returns(Promise.resolve()) };
+      const logger = makeLogger(eventCoordinator, http);
       const seed = ["google.com", "youtube.com", "facebook.com"];
-      const domains = new Domains(seed, eventCoordinator);
+      const domains = new Domains(seed, eventCoordinator, fs, logger);
 
       expect(await domains.getNextUrlToScrape()).to.equal("http://google.com");
       expect(await domains.getNextUrlToScrape()).to.equal("http://youtube.com");
