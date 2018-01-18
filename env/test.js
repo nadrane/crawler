cae8e533f949397505993aece88a3231595eea14
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-18 13:05:37 -0600

    feat(env): use custom frontier in test environment

--- env/test.js
+++ env/test.js
@@ -1,5 +1,5 @@
 const path = require("path");
 
 module.exports = {
-  FRONTIER_DIRECTORY: path.resolve(__dirname, "../frontiers")
+  FRONTIER_DIRECTORY: path.resolve(__dirname, "../test-frontiers")
 };
