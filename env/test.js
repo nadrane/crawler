2fdf51c24e4a22995e1d537e8c1d2d63a1c248e4
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-23 12:42:53 -0600

    feat(env): add logging direction config for test environment

--- env/test.js
+++ env/test.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  FRONTIER_DIRECTORY: path.resolve(__dirname, "../test-frontiers")
+  FRONTIER_DIRECTORY: path.resolve(__dirname, "../test-frontiers"),
+  LOGGING_DIR: path.resolve(__dirname, "../test-logs")
 };
