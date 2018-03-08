5dddf1eebb368df085045f3e3c0363b22b911957
Nicholas Drane <nicholasdrane@gmail.com>
2018-03-07 22:04:32 -0600

    add a couple logging messages

--- src/logger/module-loggers/requester.js
+++ src/logger/module-loggers/requester.js
@@ -2,147 +2,167 @@ const { parse } = require("tldjs");
 
 const codeModule = "requester";
 
-module.exports = logger => {
-  return {
-    streamEntered: () => {
-      logger.debug({
-        event: "stream entered",
-        codeModule
-      });
-    },
+module.exports = logger => ({
+  streamEntered: () => {
+    logger.debug({
+      event: "stream entered",
+      codeModule
+    });
+  },
 
-    streamExited: () => {
-      logger.debug({
-        event: "stream left",
-        codeModule
-      });
-    },
+  streamExited: () => {
+    logger.debug({
+      event: "stream left",
+      codeModule
+    });
+  },
 
-    badRequest: (url, config) => {
-      const { domain, subdomain } = parse(url);
-      logger.error({
-        event: "bad request",
-        codeModule,
-        url,
-        domain,
-        subdomain,
-        config
-      });
-    },
+  badRequest: (url, config) => {
+    const { domain, subdomain } = parse(url);
+    logger.error({
+      event: "bad request",
+      codeModule,
+      url,
+      domain,
+      subdomain,
+      config
+    });
+  },
 
-    requestSent: url => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "request sent",
-        codeModule,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  requestSent: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "request sent",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    responseReceived: (url, statusCode) => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "response success",
-        codeModule,
-        statusCode,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  responseReceived: (url, statusCode) => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "response success",
+      codeModule,
+      statusCode,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    responseError: (url, errMessage, status, headers) => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "response error",
-        codeModule,
-        status,
-        headers,
-        errMessage,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  responseError: (url, errMessage, status, headers) => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "response error",
+      codeModule,
+      status,
+      headers,
+      errMessage,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    noResponseRecieved: (errMessage, url) => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        errMessage,
-        event: "no response received",
-        codeModule,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  noResponseRecieved: (errMessage, url) => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      errMessage,
+      event: "no response received",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    requestTimeout: url => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "response timeout",
-        codeModule,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  requestTimeout: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "response timeout",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    connectionReset: url => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "connection reset",
-        codeModule,
-        url,
-        domain,
-        subdomain
-      });
-    },
+  connectionReset: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "connection reset",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  },
 
-    s3UploadStarted: url => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "s3 upload started",
-        codeModule,
-        domain,
-        subdomain
-      });
-    },
+  s3UploadStarted: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "s3 upload started",
+      codeModule,
+      domain,
+      subdomain
+    });
+  },
 
-    s3UploadFinished: url => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "s3 upload finished",
-        codeModule,
-        domain,
-        subdomain
-      });
-    },
+  s3UploadFinished: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "s3 upload finished",
+      codeModule,
+      domain,
+      subdomain
+    });
+  },
 
-    s3UploadError: (url, message) => {
-      const { domain, subdomain } = parse(url);
-      logger.error({
-        event: "s3 upload error",
-        codeModule,
-        url,
-        domain,
-        subdomain,
-        message
-      });
-    },
+  s3UploadError: (url, message) => {
+    const { domain, subdomain } = parse(url);
+    logger.error({
+      event: "s3 upload error",
+      codeModule,
+      url,
+      domain,
+      subdomain,
+      message
+    });
+  },
 
-    trackResponseTime: (url, responseTime) => {
-      const { domain, subdomain } = parse(url);
-      logger.info({
-        event: "track response time",
-        codeModule,
-        url,
-        domain,
-        subdomain,
-        data: responseTime
-      });
-    }
-  };
-};
+  trackResponseTime: (url, responseTime) => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "track response time",
+      codeModule,
+      url,
+      domain,
+      subdomain,
+      data: responseTime
+    });
+  },
+
+  responseStreamTimeout: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "response stream timeout",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  },
+
+  responseStreamError: url => {
+    const { domain, subdomain } = parse(url);
+    logger.info({
+      event: "response stream timeout",
+      codeModule,
+      url,
+      domain,
+      subdomain
+    });
+  }
+});
--- src/requester/index.js
+++ src/requester/index.js
@@ -10,57 +10,74 @@ const s3Stream = require("s3-upload-stream")(s3);
 
 module.exports = function createRequesterStream(logger, http, eventCoordinator, concurrency) {
   const crawlWithGetRequest = makeRequester(logger, http);
-  return throughConcurrent(logger, "requester stream", concurrency, async (requestUrl, enc, done) => {
-    logger.requester.streamEntered();
-    let htmlStream;
-    try {
-      htmlStream = await crawlWithGetRequest(requestUrl);
-    } catch (err) {
-      logger.requester.unexpectError(err, "requester stream failure");
-    }
-    // In case the request failed
-    if (!htmlStream) {
+  return throughConcurrent(
+    logger,
+    "requester stream",
+    concurrency,
+    async (requestUrl, enc, done) => {
+      logger.requester.streamEntered();
+      let htmlStream;
+      try {
+        htmlStream = await crawlWithGetRequest(requestUrl);
+      } catch (err) {
+        logger.requester.unexpectError(err, "requester stream failure");
+      }
+      // In case the request failed
+      if (!htmlStream) {
+        logger.requester.streamExited();
+        done();
+        return;
+      }
+
+      const parserStream = new Parser(requestUrl, eventCoordinator, logger);
+      htmlStream.pipe(parserStream);
+
+      // logger.requester.s3UploadStarted(requestUrl);
+      // const upload = s3Stream.upload({
+      //   Bucket: "crawler-nick",
+      //   Key: `sites/${sha1(requestUrl)}`
+      // });
+
+      // htmlStream.pipe(upload);
+
+      // const uploadFinished = new Promise((resolve, reject) => {
+      //   upload.on("uploaded", () => {
+      //     resolve();
+      //     logger.requester.s3UploadFinished(requestUrl);
+      //   });
+      //   upload.on("error", err => {
+      //     reject();
+      //     logger.requester.s3UploadError(requestUrl, err.message);
+      //   });
+      // });
+
+      const responseClosed = new Promise((resolve, reject) => {
+        // the streams file descriptor should close on this event
+        // stream is an instance of httpClientRequest and does not email a close event
+        const tenSeconds = 10 * 1000;
+        const timeoutId = setTimeout(() => {
+          logger.requester.responseStreamTimeout(requestUrl);
+          reject();
+        }, tenSeconds);
+
+        htmlStream.on("end", () => {
+          clearTimeout(timeoutId);
+          resolve();
+        });
+        htmlStream.on("error", () => {
+          logger.requester.responseStreamError(requestUrl);
+          reject();
+        });
+      });
+
+      try {
+        await responseClosed; // Promise.all([responseClosed, uploadFinished]);
+      } catch (err) {
+        console.log("final promise failed to resolve");
+        console.error(err);
+      }
       logger.requester.streamExited();
       done();
-      return;
     }
-
-    const responseClosed = new Promise(resolve => {
-      // the streams file descriptor should close on this event
-      // stream is an instance of httpClientRequest and does not email a close event
-      htmlStream.on("end", () => {
-        resolve();
-      });
-    });
-
-    const parserStream = new Parser(requestUrl, eventCoordinator, logger);
-    htmlStream.pipe(parserStream);
-
-    logger.requester.s3UploadStarted(requestUrl);
-    const upload = s3Stream.upload({
-      Bucket: "crawler-nick",
-      Key: `sites/${sha1(requestUrl)}`
-    });
-
-    htmlStream.pipe(upload);
-    const uploadFinished = new Promise((resolve, reject) => {
-      upload.on("uploaded", () => {
-        resolve();
-        logger.requester.s3UploadFinished(requestUrl);
-      });
-      upload.on("error", err => {
-        reject();
-        logger.requester.s3UploadError(requestUrl, err.message);
-      });
-    });
-    Promise.all([responseClosed, uploadFinished])
-      .then(() => {
-        logger.requester.streamExited();
-        done();
-      })
-      .catch(() => {
-        logger.requester.streamExited();
-        done();
-      });
-  });
+  );
 };
