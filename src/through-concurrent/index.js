2576bb8d345500cf434f1ee79e54d1b5b192e28d
Nicholas Drane <nicholasdrane@gmail.com>
2018-04-17 11:13:53 -0500

    fix(through-concurrency) - the event loop gets blocked if the CB to through-concurrent is not always async. Ensure it is

--- src/through-concurrent/index.js
+++ src/through-concurrent/index.js
@@ -1,7 +1,15 @@
 const throughConcurrent = require("through2-concurrent");
 
 module.exports = function createThroughConcurrent(logger, name, concurrency, callback) {
-  const stream = throughConcurrent.obj({ maxConcurrency: concurrency }, callback);
+  const stream = throughConcurrent.obj({ maxConcurrency: concurrency }, function(
+    domain,
+    enc,
+    done
+  ) {
+    setImmediate(() => {
+      callback.call(this, domain, enc, done);
+    });
+  });
   stream.on("error", err => {
     logger.unexpectedError(err, name);
   });
