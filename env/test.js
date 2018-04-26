f5d7a8bc65d2cc7d411ab39686fa6e0ec2ac36b2
Nicholas Drane <nicholasdrane@gmail.com>
2018-04-26 09:56:17 -0500

    append flush time is short for testing

--- env/test.js
+++ env/test.js
@@ -4,5 +4,5 @@ module.exports = {
   FRONTIER_DIRECTORY: path.resolve(__dirname, "../test-frontiers"),
   LOGGING_DIR: "",
   DOMAIN_REQUEST_TIME_INTERVAL: 10,
-  APPEND_FLUSH_TIME: 60 * 1000
+  APPEND_FLUSH_TIME: 2000
 };
