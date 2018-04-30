1375b0221701f0e73b7c3c1745362d2620b0c723
Nicholas Drane <nicholasdrane@gmail.com>
2018-04-30 10:17:04 -0500

    update default settings

--- env/defaults.js
+++ env/defaults.js
@@ -11,5 +11,8 @@ module.exports = {
   LOG_STREAM_BUFFER_SIZE: 5000,
   GET_REQUEST_TIMEOUT: 1500,
   ROBOTS_REQUEST_TIMEOUT: 1500,
-  APPEND_FLUSH_TIME: 2000 // Not possible to set this below 2000 milliseconds
+  APPEND_FLUSH_TIME: 2000, // Not possible to set this below 2000 milliseconds
+  MAX_CONTENT_LENGTH: 1 * 1000 * 1000, // max body length of 1 million bytes (~1mb)
+  DNS_CACHE_SIZE: 20000,
+  DOMAIN_READ_DELAY: 50
 };
